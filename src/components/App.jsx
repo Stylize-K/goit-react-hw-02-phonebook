2535f855b2d3bf54222ff8d9b359194049efac0a
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-09 23:59:04 +0300

    Add base logic to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [{ id: 'id-1', name: 'Rosie Simpson' }],
+    name: '',
+  };
+
+  generetedId = () => {
+    return nanoid();
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    const { contacts, name } = this.state;
+    this.setState({
+      contacts: [...contacts, { id: this.generetedId(), name: name }],
+    });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor={this.generetedId()}>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contact</h2>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
