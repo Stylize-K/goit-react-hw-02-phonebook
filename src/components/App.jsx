6fc88e8385b65735674c08a41718f8a64c249fb1
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-10 04:57:53 +0300

    Create separate component Filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
@@ -60,15 +61,7 @@ export class App extends Component {
       >
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
-        <label>
-          Find contacts by name
-          <input
-            type="text"
-            name="filter"
-            value={this.state.filter}
-            onChange={this.handleChangeFilter}
-          />
-        </label>
+        <Filter value={this.state.filter} onChange={this.handleChangeFilter} />
         <h2>Contacts</h2>
         <ul>
           {filteredContacts.map(contact => (
