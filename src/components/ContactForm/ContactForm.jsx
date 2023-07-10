bdfd69bbfdf157865b614d4465eabb14bddf9b2f
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-10 21:42:10 +0300

    Change validation pattern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -35,8 +35,7 @@ export class ContactForm extends Component {
             name="name"
             value={this.state.name}
             onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я]+)*$"
-            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
